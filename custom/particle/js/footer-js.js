422505682a5fdaba86d1b1088cc688b2b510efa3
Deepa L <deepa.l@zyxware.com>
2016-12-23 16:21:15 +0530

    Updated the theming as per the design.

--- custom/particle/js/footer-js.js
+++ custom/particle/js/footer-js.js
@@ -1,11 +1,4 @@
- $(document).ready(function(){
-     $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
+jQuery(function ($){
         // scroll body to 0px on click
         $('#back-to-top').click(function () {
             $('#back-to-top').tooltip('hide');
@@ -15,5 +8,4 @@
             return false;
         });
         
-        $('#back-to-top').tooltip('show');
 });
